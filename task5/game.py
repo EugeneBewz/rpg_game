93bae586b8ced0d90b1c6efaf6fc6bc50f928006
Eugene Bewz <bevz.pn@ucu.edu.ua>
2023-03-12 18:06:52 +0200

    Basic files created

---
+++ task5/game.py
@@ -0,0 +1,27 @@
+"""
+RPG game
+"""
+
+class Room:
+    pass
+
+
+class Character:
+    pass
+
+
+class Enemy:
+    pass
+
+
+class Ally:
+    pass
+
+
+class Item:
+    pass
+
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testmod())
